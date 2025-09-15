f9b8c1def7a1b0270c26a5720d2a7a1469ea76bd
Campbell <500197572@fvtc.edu>
2025-09-15 16:04:38 -0500

    Bio, image, favorites list

---
+++ site.js
@@ -0,0 +1,20 @@
+
+
+const h1 = document.querySelector('h1')
+h1.style.border = "3px solid #000000"
+h1.style.textAlign = "center"
+
+
+document.body.style.backgroundColor = "#e0a0f7ff"
+document.body.style.margin = "40px"
+document.body.style.fontFamily = "Papyrus" 
+
+const h2 = document.querySelectorAll("h2")
+for (let i = 0; i < h2.length; i++) {
+    h2[i].style.fontStyle = "italic"
+}
+
+const img = document.querySelector("img")
+img.style.margin = "15px"
+img.style.border = "4px dashed #ffffff"
+
