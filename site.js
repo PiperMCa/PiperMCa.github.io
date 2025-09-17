13a02bfb8c2e1740b6b3b7e3b614b768484d5c5b
Campbell <500197572@fvtc.edu>
2025-09-17 17:48:50 -0500

    Added welcome div

--- site.js
+++ site.js
@@ -18,3 +18,22 @@ const img = document.querySelector("img")
 img.style.margin = "15px"
 img.style.border = "4px dashed #ffffff"
 
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+const welcome = document.querySelector("div p")
+if (isMorning) {
+    welcome = "Top of the mornin to ya!"
+}
+else if (isAfternoon) {
+    welcome.textContent = "Howdy, partner - it's a fine afternoon!"
+}
+else if (isEvening) {
+    welcome.textContent = "Dusk salutations"
+}
+else {
+    welcome.textContent = "error"
+}
